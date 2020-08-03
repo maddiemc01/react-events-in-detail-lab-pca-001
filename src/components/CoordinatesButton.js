6d6977c6b1d7f37a4013e8e1c96d1c78ccfce92c
Madison McCardle <madison.mccardle@powerhrg.com>
2020-08-03 14:25:01 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinateButton extends Component {
+
+  arrayHandler = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+
+  render() {
+    return(
+      <button
+        onClick={this.arrayHandler}
+      ></button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+  // setTimeout = () => {
+  //   this.props.delay
+  // }
+
+  eventHandler = (event) => {
+    event.persist()
+    // will pass the click event to the onDelayedClick prop within a setTimeout()
+    // this.props.onDelayedClick(event)
+    setTimeout( () => this.props.onDelayedClick(event), this.props.delay)
+  }
+  render(){
+    return (
+     <button
+      onClick={this.eventHandler}
+     ></button>
+    )
+  }
+}
